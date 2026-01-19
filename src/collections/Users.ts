89311d05b8414dcebd2735bce4b06c3015667ef1
Long Tran Quang <longtrq26@gmail.com>
2026-01-20 00:31:50 +0700

    chore: setup prettier and formatting rules

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,10 +2,11 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  auth: true,
+
   admin: {
     useAsTitle: 'email',
   },
-  auth: true,
   fields: [
     // Email added by default
     // Add more fields as needed
