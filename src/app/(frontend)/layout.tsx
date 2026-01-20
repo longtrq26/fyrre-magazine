4ca5b1ed1963712041685a350515fd9c678eaed5
Long Tran Quang <longtrq26@gmail.com>
2026-01-20 12:49:44 +0700

    feat: scaffold frontend structure and auth routes

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,12 +1,12 @@
+import '@/app/globals.css'
 import React from 'react'
-import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
+export default async function Layout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
