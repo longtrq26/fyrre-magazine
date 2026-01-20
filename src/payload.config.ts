83f11d72fdc3955ee1c975c99e6e0118eb19102c
Long Tran Quang <longtrq26@gmail.com>
2026-01-20 12:04:16 +0700

    feat: add storage and email configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,11 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
-
 import { Categories } from './collections/categories/config'
 import { Media } from './collections/media/config'
 import { Posts } from './collections/posts/config'
@@ -16,23 +17,68 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  // administrative & security
   admin: {
     user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
     },
   },
+  secret: process.env.PAYLOAD_SECRET!,
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL!,
+  cors: [process.env.NEXT_PUBLIC_SERVER_URL!].filter(Boolean),
+  csrf: [process.env.NEXT_PUBLIC_SERVER_URL!].filter(Boolean),
+
+  // content schema
   collections: [Users, Media, Posts, Categories, Tags, Subscribers],
+  globals: [],
+
+  // editor & image processing
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  sharp,
+
+  // database
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL!,
+    },
+  }),
+
+  // external services
+  email: nodemailerAdapter({
+    defaultFromAddress: process.env.EMAIL_FROM_ADDRESS!,
+    defaultFromName: process.env.EMAIL_FROM_NAME!,
+    transportOptions: {
+      host: process.env.EMAIL_HOST!,
+      port: Number(process.env.EMAIL_PORT || 587),
+      secure: process.env.EMAIL_SECURE || false,
+      auth: {
+        user: process.env.EMAIL_USER!,
+        pass: process.env.EMAIL_PASS!,
+      },
+    },
+  }),
+
+  // plugins
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET_NAME!,
+      config: {
+        endpoint: process.env.S3_ENDPOINT!,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.S3_REGION || 'auto',
+      },
+    }),
+  ],
+
+  // development & meta
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URL || '',
-    },
-  }),
-  sharp,
-  plugins: [],
 })
