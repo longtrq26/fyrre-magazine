3f8925c8a1e118a2e00cfe31ab5eed91c12aec12
Long Tran Quang <longtrq26@gmail.com>
2026-01-20 14:34:46 +0700

    chore: add font assets and global typography setup

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,39 @@
 import '@/app/globals.css'
+import localFont from 'next/font/local'
 import React from 'react'
+import { Toaster } from 'sonner'
+
+const generalSans = localFont({
+  src: [
+    {
+      path: '../../assets/fonts/GeneralSans/Fonts/WEB/fonts/GeneralSans-Variable.woff2',
+      weight: '100 900',
+      style: 'normal',
+    },
+    {
+      path: '../../assets/fonts/GeneralSans/Fonts/WEB/fonts/GeneralSans-VariableItalic.woff2',
+      weight: '100 900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-general-sans',
+})
+
+const satoshi = localFont({
+  src: [
+    {
+      path: '../../assets/fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Variable.woff2',
+      weight: '100 900',
+      style: 'normal',
+    },
+    {
+      path: '../../assets/fonts/Satoshi/Fonts/WEB/fonts/Satoshi-VariableItalic.woff2',
+      weight: '100 900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-satoshi',
+})
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -10,9 +44,10 @@ export default async function Layout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="en" className={`${generalSans.variable} ${satoshi.variable} antialiased`}>
       <body>
         <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   )
